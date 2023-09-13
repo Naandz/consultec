5a74e5c48b3fdf58dad0bc795291b3cbf2e2305c
Nicolas Lacerda <nicolaasxs@gmail.com>
2023-09-12 21:24:48 -0300

    feat: implementações na tela de login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,28 @@
+import { Button, Card, Input, Text } from "@mantine/core";
+import styles from "./Login.module.css";
 export default function Login() {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className={styles.container}>
+      <div>
+        <h1>Consultec</h1>
+        <Text color="#757578">
+          Contabilidade Profissional, Soluções Sólidas!
+        </Text>
+      </div>
+      <div className={styles.card}>
+        <Card shadow="sm" p="lg">
+          <Input placeholder="E-mail" />
+          <Input placeholder="Senha" />
+          <Button
+            variant="gradient"
+            gradient={{ from: "#2256f2", to: "#4674FF" }}
+            fullWidth
+          >
+            Entrar
+          </Button>
+        </Card>
+        
+      </div>
     </div>
   );
 }
