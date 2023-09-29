6296258e5256c6ac36f5bdccb53b4fcda9334f35
Nicolas Lacerda <nicolaasxs@gmail.com>
2023-09-28 22:43:42 -0300

    feat: adiciona cadastro jurídico

--- src/pages/Juridica/CadastroJuridica.tsx
+++ src/pages/Juridica/CadastroJuridica.tsx
@@ -1,150 +1,190 @@
 import { Button, Flex, Input, Switch } from "@mantine/core";
+import { isNotEmpty, useForm } from "@mantine/form";
 import style from "./Cadastro.module.css";
 
 export default function CadastroJuridica() {
+  const form = useForm({
+    initialValues: {
+      contrato: "",
+      razaoSocial: "",
+      cnpj: "",
+      ramoatividade: "",
+    },
+    validate: {
+      contrato: isNotEmpty("Contrato"),
+      razaoSocial: isNotEmpty("Razão Social"),
+      cnpj: isNotEmpty("CNPJ"),
+      ramoatividade: isNotEmpty("Ramo de Atividade"),
+    },
+  });
+
+  const cadastra = async ({
+    contrato,
+    razaoSocial,
+    cnpj,
+    ramoatividade,
+  }: {
+    contrato: string;
+    razaoSocial: string;
+    cnpj: string;
+    ramoatividade: string;
+  }) => {
+    console.log({ contrato, razaoSocial, cnpj, ramoatividade });
+  };
+
   return (
     <div>
-      <Flex justify="center" align="start">
-        <Flex justify="center" align="center" direction="column" rowGap="xl">
-          <Flex columnGap="5em" rowGap="xs" direction="row" wrap="wrap">
-            <Flex justify="center" align="start" gap="xs" direction="column">
-              <Flex>
-                <Switch.Group label="Contrato" withAsterisk size="sm">
-                  <Switch value="Contrato" />
-                </Switch.Group>
+      <form onSubmit={form.onSubmit(cadastra)}>
+        <Flex justify="center" align="start">
+          <Flex justify="center" align="center" direction="column" rowGap="xl">
+            <Flex columnGap="5em" rowGap="xs" direction="row" wrap="wrap">
+              <Flex justify="center" align="start" gap="xs" direction="column">
+                <Flex>
+                  <Switch.Group
+                    label="Contrato"
+                    withAsterisk
+                    size="sm"
+                    {...form.getInputProps("contrato")}
+                  >
+                    <Switch value="Contrato" />
+                  </Switch.Group>
+                </Flex>
+                <div>
+                  <label htmlFor="Razão Social">Razão Social</label>
+                  <Input
+                    name="Razão Social"
+                    placeholder="Informe a razão social"
+                    className={style.inputCamp}
+                    {...form.getInputProps("razaoSocial")}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="CPF">CNPJ</label>
+                  <Input
+                    name="CNPJ"
+                    placeholder="Informe o CNPJ"
+                    className={style.inputCamp}
+                    {...form.getInputProps("cnpj")}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="Ramo de Atividade">Ramo de Atividade</label>
+                  <Input
+                    name="Ramo de Atividade"
+                    placeholder="Informe o ramo de atividade"
+                    className={style.inputCamp}
+                    {...form.getInputProps("ramoatividade")}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="CEP">CEP</label>
+                  <Input
+                    name="CEP"
+                    placeholder="Informe o CEP"
+                    className={style.inputCamp}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="Bairro">Bairro</label>
+                  <Input
+                    name="Bairro"
+                    placeholder="Informe o Bairro"
+                    className={style.inputCamp}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="logradouro">logradouro</label>
+                  <Input
+                    name="logradouro"
+                    placeholder="Informe o logradouro"
+                    className={style.inputCamp}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="Usuário Sefaz">Usuário Sefaz</label>
+                  <Input
+                    name="Usuário Sefaz"
+                    placeholder="Login"
+                    className={style.inputCamp}
+                  />
+                </div>
               </Flex>
-              <div>
-                <label htmlFor="Razão Social">Razão Social</label>
-                <Input
-                  name="Razão Social"
-                  placeholder="Informe a razão social"
-                  className={style.inputCamp}
-                />
-              </div>
-              <div>
-                <label htmlFor="CPF">CNPJ</label>
-                <Input
-                  name="CNPJ"
-                  placeholder="Informe o CNPJ"
-                  className={style.inputCamp}
-                />
-              </div>
-              <div>
-                <label htmlFor="Ramo de Atividade">Ramo de Atividade</label>
-                <Input
-                  name="Ramo de Atividade"
-                  placeholder="Informe o ramo de atividade"
-                  className={style.inputCamp}
-                />
-              </div>
-              <div>
-                <label htmlFor="CEP">CEP</label>
-                <Input
-                  name="CEP"
-                  placeholder="Informe o CEP"
-                  className={style.inputCamp}
-                />
-              </div>
-              <div>
-                <label htmlFor="Bairro">Bairro</label>
-                <Input
-                  name="Bairro"
-                  placeholder="Informe o Bairro"
-                  className={style.inputCamp}
-                />
-              </div>
-              <div>
-                <label htmlFor="logradouro">logradouro</label>
-                <Input
-                  name="logradouro"
-                  placeholder="Informe o logradouro"
-                  className={style.inputCamp}
-                />
-              </div>
-              <div>
-                <label htmlFor="Usuário Sefaz">Usuário Sefaz</label>
-                <Input
-                  name="Usuário Sefaz"
-                  placeholder="Login"
-                  className={style.inputCamp}
-                />
-              </div>
-            </Flex>
-            <Flex justify="center" align="center" gap="xs" direction="column">
-              <Flex align="center" columnGap="5.4em" direction="row">
-                <Switch.Group label="Fiscal" size="sm">
-                  <Switch value="fiscal" />
-                </Switch.Group>
-                <Switch.Group label="Pessoal" size="sm">
-                  <Switch value="pessoal" />
-                </Switch.Group>
-                <Switch.Group label="Contábil" size="sm">
-                  <Switch value="contabil" />
-                </Switch.Group>
+              <Flex justify="center" align="center" gap="xs" direction="column">
+                <Flex align="center" columnGap="5.4em" direction="row">
+                  <Switch.Group label="Fiscal" size="sm">
+                    <Switch value="fiscal" />
+                  </Switch.Group>
+                  <Switch.Group label="Pessoal" size="sm">
+                    <Switch value="pessoal" />
+                  </Switch.Group>
+                  <Switch.Group label="Contábil" size="sm">
+                    <Switch value="contabil" />
+                  </Switch.Group>
+                </Flex>
+                <div>
+                  <label htmlFor="Nome Fantasia">Nome Fantasia</label>
+                  <Input
+                    name="Nome Fantasia"
+                    placeholder="Informe o nome fantasia"
+                    className={style.inputCamp}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="Telefone">Telefone</label>
+                  <Input
+                    name="Telefone"
+                    placeholder="Informe o telefone"
+                    className={style.inputCamp}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="E-mail">E-mail</label>
+                  <Input
+                    name="E-mail"
+                    placeholder="Informe o e-mail"
+                    className={style.inputCamp}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="Cidade">Cidade</label>
+                  <Input
+                    name="Cidade"
+                    placeholder="Informe o cidade"
+                    className={style.inputCamp}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="Estado">Estado</label>
+                  <Input
+                    name="Estado"
+                    placeholder="Informe o estado"
+                    className={style.inputCamp}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="Numero">Numero</label>
+                  <Input
+                    name="Numero"
+                    placeholder="Informe o numero"
+                    className={style.inputCamp}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="Senha Sefaz">Senha Sefaz</label>
+                  <Input
+                    name="Senha Sefaz"
+                    placeholder="Senha"
+                    className={style.inputCamp}
+                  />
+                </div>
               </Flex>
-              <div>
-                <label htmlFor="Nome Fantasia">Nome Fantasia</label>
-                <Input
-                  name="Nome Fantasia"
-                  placeholder="Informe o nome fantasia"
-                  className={style.inputCamp}
-                />
-              </div>
-              <div>
-                <label htmlFor="Telefone">Telefone</label>
-                <Input
-                  name="Telefone"
-                  placeholder="Informe o telefone"
-                  className={style.inputCamp}
-                />
-              </div>
-              <div>
-                <label htmlFor="E-mail">E-mail</label>
-                <Input
-                  name="E-mail"
-                  placeholder="Informe o e-mail"
-                  className={style.inputCamp}
-                />
-              </div>
-              <div>
-                <label htmlFor="Cidade">Cidade</label>
-                <Input
-                  name="Cidade"
-                  placeholder="Informe o cidade"
-                  className={style.inputCamp}
-                />
-              </div>
-              <div>
-                <label htmlFor="Estado">Estado</label>
-                <Input
-                  name="Estado"
-                  placeholder="Informe o estado"
-                  className={style.inputCamp}
-                />
-              </div>
-              <div>
-                <label htmlFor="Numero">Numero</label>
-                <Input
-                  name="Numero"
-                  placeholder="Informe o numero"
-                  className={style.inputCamp}
-                />
-              </div>
-              <div>
-                <label htmlFor="Senha Sefaz">Senha Sefaz</label>
-                <Input
-                  name="Senha Sefaz"
-                  placeholder="Senha"
-                  className={style.inputCamp}
-                />
-              </div>
             </Flex>
+            <Button fullWidth className="botao" type="submit">
+              Salvar
+            </Button>
           </Flex>
-          <Button fullWidth className="botao">
-            Salvar
-          </Button>
         </Flex>
-      </Flex>
+      </form>
     </div>
   );
 }
