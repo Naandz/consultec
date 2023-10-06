330e43678c115e6b61503e5317110d27db6f15ea
Nicolas <nsdev14@hotmail.com>
2023-10-06 14:31:14 -0300

    feat: adiciona pagem 404

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,11 +1,11 @@
-import { Button, Flex } from "@mantine/core";
+import { Button, Flex, Title } from "@mantine/core";
 import { useNavigate } from "react-router-dom";
 
 export default function NotFound() {
   const navigate = useNavigate();
   return (
     <Flex align="center" justify="center" direction="column">
-      <h1 className="NotFound">404</h1>
+      <Title color="green">404</Title>
       <h2>Página não encontrada</h2>
       <p>Desculpe, a página que você está procurando não foi encontrada.</p>
       <Button className="botao" onClick={() => navigate("/")}>
