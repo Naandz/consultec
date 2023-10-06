5dae8d2eb222f526ec862b9cc0f742d60c919963
Nicolas <nsdev14@hotmail.com>
2023-10-06 14:42:17 -0300

    refactor: refatoração do css

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -4,8 +4,8 @@ import { useNavigate } from "react-router-dom";
 export default function NotFound() {
   const navigate = useNavigate();
   return (
-    <Flex align="center" justify="center" direction="column">
-      <Title color="green">404</Title>
+    <Flex align="center" justify="center" direction="column" gap={14}>
+      <Title color="blue">404</Title>
       <h2>Página não encontrada</h2>
       <p>Desculpe, a página que você está procurando não foi encontrada.</p>
       <Button className="botao" onClick={() => navigate("/")}>
