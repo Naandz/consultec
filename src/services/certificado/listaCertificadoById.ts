5cc418d04df1cbac0fa115782583cd5f8a9a3e80
Nicolas <nsdev14@hotmail.com>
2023-10-06 16:24:25 -0300

    teste: vercel

--- src/services/certificado/listaCertificadoById.ts
+++ src/services/certificado/listaCertificadoById.ts
@@ -2,7 +2,7 @@ import api from "../api";
 
 import { Certificado } from "./Certificado";
 
-const listaCertificadoById = async (id?: string) => {
+const listaCertificadoById = async (id: string) => {
   const { data } = await api.get<Certificado[]>(
     `/clients/certificate/${id}`
   );
